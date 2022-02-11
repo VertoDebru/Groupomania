e9235f10e1a27a65875ae7313d446d6bee10d84d
VertoDebru <t.vervoot@gmail.com>
2022-02-11 18:07:51 +0100

    Add components in main.

--- groupomania_front/src/components/Main.js
+++ groupomania_front/src/components/Main.js
@@ -1,14 +1,12 @@
 import "../styles/Main.css";
+import PostFormArticle from "./PostFormArticle";
+import Article from "./Article";
 
 function Main() {
   return (
     <div className='app-main'>
-        <h2>Last posts :</h2>
-        <article>
-            <h2>Post Title</h2>
-            <h3>Time here</h3>
-            <p>Veniam ad deserunt non culpa esse nisi amet dolor proident amet cupidatat laborum...</p>
-        </article>
+      <PostFormArticle />
+      <Article />
     </div>
   );
 }
