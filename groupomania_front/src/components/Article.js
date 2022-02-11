c3824d08f3aac2a57c24047cfd641776c6c81236
VertoDebru <t.vervoot@gmail.com>
2022-02-11 17:25:01 +0100

    Add like & comments in article component.

--- groupomania_front/src/components/Article.js
+++ groupomania_front/src/components/Article.js
@@ -8,6 +8,10 @@ function Article() {
       <h2>Post Title</h2>
       <h3>Verto Debru | 32 min</h3>
       <p>Veniam ad deserunt non culpa esse nisi amet dolor proident amet cupidatat laborum...</p>
+      <a href='#' className='like'>
+        <i className='fa-solid fa-thumbs-up'></i> 2 likes
+      </a>
+      <a href='#'>2 comments</a>
     </article>
   );
 }
