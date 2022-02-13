43c6ae1f13f7325b90544c1c61d08318f51e00d9
VertoDebru <t.vervoot@gmail.com>
2022-02-13 16:10:23 +0100

    Add avatar component.

---
+++ groupomania_front/src/components/Avatar.js
@@ -0,0 +1,11 @@
+import "../styles/Avatar.css";
+
+function Avatar() {
+  return (
+    <div className='app-avatar'>
+      <i class="fa-solid fa-user"></i>
+    </div>
+  );
+}
+
+export default Avatar;
