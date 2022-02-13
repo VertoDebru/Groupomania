b033f4aedf68697893251bff3df4b9c3483002de
VertoDebru <t.vervoot@gmail.com>
2022-02-13 15:34:23 +0100

    Remove PostFormArticle component.

--- groupomania_front/src/components/Main.js
+++ groupomania_front/src/components/Main.js
@@ -1,11 +1,9 @@
 import "../styles/Main.css";
-import PostFormArticle from "./PostFormArticle";
 import Article from "./Article";
 
 function Main() {
   return (
     <div className='app-main'>
-      <PostFormArticle />
       <Article />
     </div>
   );
