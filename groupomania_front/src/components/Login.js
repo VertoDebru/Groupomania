865c13b5cb1de5e1eb428ce71a735fc90cece74e
VertoDebru <t.vervoot@gmail.com>
2022-02-13 18:14:29 +0100

    Finish login component layout.

--- groupomania_front/src/components/Login.js
+++ groupomania_front/src/components/Login.js
@@ -1,15 +1,25 @@
 import "../styles/Login.css";
 
+const btnValueLogin = "Connexion";
+const btnValueSign = "Inscription";
+
 function Login() {
   return (
     <div className='app-login'>
       <div className='app-user-icon fa-solid fa-user-secret'></div>
       <form method='post' action='#' className='app-login-form'>
-        <input type='text' placeholder='Username' required />
-        <input type='password' placeholder='Password' required />
-        <input type='button' value='Log me' />
-        <a href='#'>Sign in</a>
+        <h3>Email</h3>
+        <input type='text' placeholder='Ex : example@groupomania.com' required />
+        <h3>Mot de passe</h3>
+        <input type='password' placeholder='mot de passe' required />
+        <input type='button' className='login' value={btnValueLogin} />
       </form>
+      
+      <div className='app-sign'>
+        <h2>Vous n'êtes pas encore membre ?</h2>
+        <input type='button' className='sign' value={btnValueSign} />
+      </div>
+
     </div>
   );
 }
