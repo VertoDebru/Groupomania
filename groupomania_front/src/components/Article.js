1c7ab1765ccc6c3959c4ef614d2e0dfdb375e8d1
VertoDebru <t.vervoot@gmail.com>
2022-02-13 16:55:30 +0100

    Update article component.

--- groupomania_front/src/components/Article.js
+++ groupomania_front/src/components/Article.js
@@ -1,17 +1,23 @@
+import Avatar from "./Avatar";
 import "../styles/Article.css";
-import avatar from "../assets/logo192.png";
+/*import avatar from "../assets/logo192.png";*/
 
 function Article() {
   return (
     <article>
-      <img className='avatar' src={avatar} />
-      <h2>Post Title</h2>
-      <h3>Verto Debru | 32 min</h3>
+      <div className='header'>
+        {/*<img className='avatar' src={avatar} alt='avatar' />*/}
+        <Avatar />
+        <div>
+          <h2>Post Title</h2>
+          <h3>Verto Debru | <i class="fa-solid fa-clock"></i> 32 min</h3>
+        </div>
+      </div>
       <p>Veniam ad deserunt non culpa esse nisi amet dolor proident amet cupidatat laborum...</p>
-      <a href='#' className='like'>
+      <a href className='like'>
         <i className='fa-solid fa-thumbs-up'></i> 2 likes
       </a>
-      <a href='#'>2 comments</a>
+      <a href>2 comments</a>
     </article>
   );
 }
