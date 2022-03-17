e7bcf2bf6849efac236b78f8e7bc5fc35aed9c28
VertoDebru <t.vervoot@gmail.com>
2022-03-17 18:38:30 +0100

    Convert to class.

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -1,43 +1,39 @@
+import React from "react";
 // Import components
 import Header from "./Header";
 import Main from "./Main";
 import Login from "./Login";
 import Footer from "./Footer";
 
-/*         Token        *
-  * ******************* */
-function getToken() {
-  const userToken = sessionStorage.getItem('token');
-  return userToken;
-}
-/* ******************* *
- *       END Token     */
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.token = sessionStorage.getItem('token');
+  }
 
-/*         Login        *
- * ******************** */
-function checkLogin() {
-  const token = getToken();
-  if(token) {
+  setApp() {
+    if(this.token) {
+      return (
+        <div>
+          <Header />
+          <Main />
+          <Footer />
+        </div>
+      )
+    }
+    else {
+      return (
+        <div>
+          <Header />
+          <Login />
+        </div>
+      )
+    }
+  }
+
+  render() {
     return (
-      <div>
-        <Header />
-        <Main />
-        <Footer />
-      </div>
+      this.setApp()
     )
   }
-  else {
-    return (
-      <div>
-        <Header />
-        <Login />
-      </div>
-    )
-  }
-}
-/* ******************* *
- *      END Login      */
-
-export default function App() {
-  return checkLogin();
 }
