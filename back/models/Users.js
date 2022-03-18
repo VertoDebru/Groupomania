3cef4abf3a0498a6d9346c96c1e916a9ff0de22f
VertoDebru <t.vervoot@gmail.com>
2022-03-18 16:04:10 +0100

    Add avatar column.

--- back/models/Users.js
+++ back/models/Users.js
@@ -22,6 +22,10 @@ module.exports = (sequelize, Sequelize) => {
             type: Sequelize.INTEGER,
             defaultValue: 0
         },
+        avatar: {
+            type: Sequelize.STRING,
+            defaultValue: "none"
+        },
         isAdmin: {
             type: Sequelize.BOOLEAN,
             defaultValue: false
