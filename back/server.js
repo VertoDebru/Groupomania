baf4c177675ff63b830e0fc2e931b1cf81cd66e4
VertoDebru <t.vervoot@gmail.com>
2022-03-18 16:05:04 +0100

    Add force true for create table if not exist.

--- back/server.js
+++ back/server.js
@@ -20,7 +20,7 @@ server.listen(MY_PORT, () => {
     console.log('Checking database connection...');
     db.sequelize.authenticate()
     .then( () => {
-        db.sequelize.sync();
+        db.sequelize.sync({force: true});
         console.log('Database connection OK!');
         console.log('-----------------------');
     })
