58ea43047faef2d67d362b42c99910dbfc13275c
VertoDebru <t.vervoot@gmail.com>
2022-03-18 15:30:03 +0100

    Convert to class.

--- front/src/routes/Register.js
+++ front/src/routes/Register.js
@@ -1,75 +1,87 @@
+import React from "react";
 import axios from "axios";
 import "../styles/Login.css";
 // Import components
 import Header from "../components/Header";
 
-const btnValueLogin = "Connexion";
-const btnValueSign = "Inscription";
-const reqSign = "http://localhost:8080/api/auth/sign";
 
-/*         Token        *
-  * ******************* */
-function setToken(userToken) {
-  sessionStorage.setItem('token', JSON.stringify(userToken));
-  document.location.href = "./../";
-}
-
-function getToken() {
-  const userToken = sessionStorage.getItem('token');
-  return userToken;
-}
-/* ******************* *
- *       END Token     */
-
-const login = ( () => {
-  document.location.href = "./../";
-})
-
-const eventOnSubmit = ( () => {
-  let inputEmail = document.getElementsByName("email")[0];
-  let inputPassword = document.getElementsByName("password")[0];
-  
-  // Controle des champs formulaire.
-  if(inputEmail.value.length <= 4 || inputPassword.value.length < 4 ) {
-    return console.error("Input not complete!");
+export default class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false
+    };
+    this.token = sessionStorage.getItem('token');
+    this.signUrl = 'http://localhost:8080/api/auth/sign';
+    // Inputs
+    this.email = null;
+    this.password = null;
+    // OnClick
+    this.postSign = this.postSign.bind(this);
+    // Check Token
+    this.checkToken = (() => this.token ? document.location.href = './../' : null);
   }
 
-  axios.post(reqSign, {
-    email: inputEmail.value,
-    password: inputPassword.value
-  })
-  .then( (res) => {
-    console.log(res.data);
-  })
-  .catch( (error) => {
-    console.log(error);
-  });
-})
+  componentDidMount() {
+    this.checkToken();
+    // Get Inputs
+    this.email = document.getElementsByName("email")[0];
+    this.password = document.getElementsByName("password")[0];
+  }
 
-export default function Register() {
-  const token = getToken();
-  if(token) return document.location.href = './../';
+  postSign() {
+    // Check inputs.
+    if(this.email.value.length <= 4 || this.password.value.length < 4 ) {
+      return console.error("Input not complete!");
+    }
+
+    this.setState({ isLoading: true });
+    // Post request.
+    axios.post(this.signUrl, {
+      email: this.email.value,
+      password: this.password.value
+    })
+    .then((res) => { 
+      this.setToken(res.data.token);
+      this.setState({ isLoading: false });
+    })
+    .catch((err) => {
+      console.log(err);
+      this.setState({ isLoading: false });
+    });
+  }
+
+  login() {
+    document.location.href = "./../";
+  }
+
+  setToken(userToken) {
+    sessionStorage.setItem('token', JSON.stringify(userToken));
+    document.location.href = "./../";
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <main>
+          <div className='app-register'>
+            <div className='app-sign-icon fa-solid fa-file-signature'></div>
+            <form method='post' className='app-sign-form'>
+              <h3>Email</h3>
+              <input name='email' type='email' placeholder='Ex : example@groupomania.com' label='Adresse email' required />
+              <h3>Mot de passe</h3>
+              <input name='password' type='password' placeholder='mot de passe' label='Mot de passe' required />
+              <input type='button' onClick={this.postSign} disabled={this.state.isLoading} className='login' label='Inscription' value='Inscription' />
+            </form>
   
-  return (
-    <div>
-      <Header />
-      <main>
-        <div className='app-register'>
-          <div className='app-sign-icon fa-solid fa-file-signature'></div>
-          <form method='post' className='app-sign-form'>
-            <h3>Email</h3>
-            <input name='email' type='email' placeholder='Ex : example@groupomania.com' label='Adresse email' required />
-            <h3>Mot de passe</h3>
-            <input name='password' type='password' placeholder='mot de passe' label='Mot de passe' required />
-            <input type='button' onClick={eventOnSubmit} className='login' label={btnValueSign} value={btnValueSign} />
-          </form>
-
-          <div className='app-sign'>
-            <h2>Vous êtes déjà membre ?</h2>
-            <input type='button' onClick={login} className='sign' label={btnValueLogin} value={btnValueLogin} />
+            <div className='app-sign'>
+              <h2>Vous êtes déjà membre ?</h2>
+              <input type='button' onClick={this.login} disabled={this.state.isLoading} className='sign' label='Connexion' value='Connexion' />
+            </div>
           </div>
-        </div>
-      </main>
-    </div>
-  );
+        </main>
+      </div>
+    )
+  }
 }
