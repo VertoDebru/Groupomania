53b6ddaff4754d03aa9b38ab8c17739f94d20466
VertoDebru <t.vervoot@gmail.com>
2022-03-19 20:41:35 +0100

    Convert to class.

--- front/src/components/Article.js
+++ front/src/components/Article.js
@@ -1,29 +1,43 @@
+import React from "react";
+//import axios from "axios";
+// Import components
 import Avatar from "./Avatar";
 import Comments from "./Comments";
 import AddComment from "./AddComment";
+
 import "../styles/Article.css";
 
-export default function Article() {
-  return (
-    <div>
-      <article>
-        <div className='header'>
-          <Avatar />
-          <div>
-            <h2>Post Title</h2>
-            <h3>Verto Debru | <i className="fa-solid fa-clock"></i> 32 min</h3>
+export default class Article extends React.Component {
+  constructor(dataArticle) {
+    super(dataArticle);
+    this.article = dataArticle.dataArticle;
+    console.log(this.article);
+    this.articleId = this.article.id;
+    console.log("Article ID : "+this.articleId);
+  }
+
+  render() {
+    return (
+      <div>
+        <article>
+          <div className='header'>
+            <Avatar userId={this.article.authorId}/>
+            <div>
+              <h2>{this.article.title}</h2>
+              <h3>Verto Debru | <i className="fa-solid fa-clock"></i> {this.article.postDate}</h3>
+            </div>
           </div>
-        </div>
-        <p>Veniam ad deserunt non culpa esse nisi amet dolor proident amet cupidatat laborum...</p>
-        <div className='footer'>
-          <a href='true' className='like'>
-            <i className='fa-solid fa-thumbs-up'></i> 2 likes
-          </a>
-          <a href='true'>2 comments</a>
-        </div>
-      </article>
-      <Comments />
-      <AddComment />
-    </div>
-  );
+          <p>{this.article.article}</p>
+          <div className='footer'>
+            <a href='true' className='like'>
+              <i className='fa-solid fa-thumbs-up'></i> 2 likes
+            </a>
+            <a href='true'>2 comments</a>
+          </div>
+        </article>
+        <Comments articleId={this.articleId} />
+        <AddComment />
+      </div>
+    )
+  }
 }
--- front/src/components/Avatar.js
+++ front/src/components/Avatar.js
@@ -1,9 +1,49 @@
+import React from "react";
+import axios from "axios";
 import "../styles/Avatar.css";
 
-export default function Avatar() {
-  return (
-    <div className='app-avatar'>
-      <i className="fa-solid fa-user"></i>
-    </div>
-  );
+export default class Avatar extends React.Component {
+  constructor(userId) {
+    super(userId);
+    this.state = {
+      isLoading: false
+    };
+    this.token = sessionStorage.getItem('token');
+    this.userId = userId.userId;
+    this.getUrl = 'http://localhost:8080/api/user?id='+this.userId;
+    this.avatar = '';
+    console.log("User ID : "+this.userId);
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    // Get Inputs
+    this.getAvatar();
+  }
+
+  getAvatar() {
+    axios.get(this.getUrl)
+    .then((res) => { 
+      this.avatar = res.data.avatar;
+      this.setState({ isLoading: false });
+    })
+    .catch(() => {
+      this.avatar = "none";
+      this.setState({ isLoading: false });
+    });
+  }
+
+  setAvatar() {
+    if(this.avatar === 'none') {
+      return <i className="fa-solid fa-user"></i>
+    }
+  }
+
+  render() {
+    return (
+        <div className='app-avatar'>
+        {this.setAvatar()}
+        </div>
+    )
+  }
 }
