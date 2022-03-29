0ce3d59cf6321d4753785c39e08488f0e04a320b
VertoDebru <t.vervoot@gmail.com>
2022-03-29 14:13:34 +0200

    Update route.

--- back/routes/comments.js
+++ back/routes/comments.js
@@ -5,7 +5,7 @@ const ctrlComment = require('../controllers/comments');
 
 router.get('/', ctrlComment.commentsGet);
 router.post('/', ctrlComment.commentAdd);
-router.put('/', ctrlComment.commentsEdit);
+router.put('/', ctrlComment.commentEdit);
 router.delete('/', ctrlComment.commentDel);
 
 module.exports = router;
