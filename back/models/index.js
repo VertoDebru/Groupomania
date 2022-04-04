71f5e566f9b11934b9e771700c5510a5ad1077b8
VertoDebru <t.vervoot@gmail.com>
2022-04-04 22:24:35 +0200

    Add associated models.

--- back/models/index.js
+++ back/models/index.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-//const dbConfig = require("../config/db.config.js");
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
@@ -18,5 +17,17 @@ db.sequelize = sequelize;
 db.users = require("./Users")(sequelize, Sequelize);
 db.articles = require("./Articles")(sequelize, Sequelize);
 db.comments = require("./Comments")(sequelize, Sequelize);
+
+// Associated models
+const keyAuthor = { name: 'authorId', allowNull: false };
+const keyArticle = { name: 'articleId', allowNull: false };
+// Articles
+db.users.hasMany(db.articles, {foreignKey: keyAuthor});
+db.articles.belongsTo(db.users, {foreignKey: keyAuthor});
+// Comments
+db.users.hasMany(db.comments, {foreignKey: keyAuthor});
+db.comments.belongsTo(db.users, {foreignKey: keyAuthor});
+db.articles.hasMany(db.comments, {foreignKey: keyArticle});
+db.comments.belongsTo(db.articles, {foreignKey: keyArticle});
 
 module.exports = db;
