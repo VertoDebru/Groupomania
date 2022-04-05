ffa027e290c01971d98c350c17cc7c7bc82cc688
VertoDebru <t.vervoot@gmail.com>
2022-04-05 17:09:28 +0200

    Update route for Profile page.

--- front/src/index.js
+++ front/src/index.js
@@ -1,10 +1,12 @@
 import { render } from 'react-dom';
 import { BrowserRouter, Routes, Route  } from "react-router-dom";
-import './styles/index.css';
 // Import components
 import Header from "./components/Header";
 import App from './components/App';
 import Register from './routes/Register';
+import Profile from './routes/Profile';
+
+import './styles/index.css';
 
 const rootElement = document.getElementById("root");
 render(
@@ -12,6 +14,7 @@ render(
   <Routes>
     <Route path="/" element={<App />} />
     <Route path="/Register" element={<Register />} />
+    <Route path="/Profile" element={<Profile />} />
     <Route path="*" element={
       <div>
         <Header />
