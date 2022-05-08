0a7f1bc13861e898bc97cce7f1e4de0c4c22818f
VertoDebru <t.vervoot@gmail.com>
2022-05-08 12:10:41 +0200

    Complete server.

--- back/server.js
+++ back/server.js
@@ -20,7 +20,7 @@ server.listen(MY_PORT, () => {
     console.log('Checking database connection...');
     db.sequelize.authenticate()
     .then( () => {
-        db.sequelize.sync({alter: true, force: false});
+        db.sequelize.sync({alter: false, force: false});
         console.log('Database connection OK!');
         console.log('-----------------------');
     })
