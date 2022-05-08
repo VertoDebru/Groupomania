9af6a6639a14478c7860552e2428227de406c88e
VertoDebru <t.vervoot@gmail.com>
2022-05-08 12:09:44 +0200

    Complete app server.

--- back/app.js
+++ back/app.js
@@ -5,6 +5,8 @@ const path = require('path');
 const userRoutes = require('./routes/users');
 const articlesRoutes = require('./routes/articles');
 const commentsRoutes = require('./routes/comments');
+const jobsRoutes = require('./routes/jobs');
+const likesRoutes = require('./routes/likes');
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -12,6 +14,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
 });
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routes
@@ -19,6 +22,8 @@ app.use('/api/auth', userRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/articles', articlesRoutes);
 app.use('/api/comments', commentsRoutes);
+app.use('/api/jobs', jobsRoutes);
+app.use('/api/likes', likesRoutes);
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/avatars', express.static(path.join(__dirname, 'avatars')));
